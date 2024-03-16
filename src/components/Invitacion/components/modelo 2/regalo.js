e35be3679b19bc3a3a0505499060050d1670b4f4
Josadac Castaneda <jugdmentc12@gmail.com>
2024-03-16 09:10:47 -0600

    err

--- src/components/Invitacion/components/modelo 2/regalo.js
+++ src/components/Invitacion/components/modelo 2/regalo.js
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 function Regalo ({regalo}){
 
     return(
-        <Link className='regalo shadow'>
+        <Link className='regalo shadow' to={`https://mesaderegalos.liverpool.com.mx/milistaderegalos/${regalo[0].codigo}`}>
             <h3 className='titulo'>Mesa de regalos</h3>
-            <div className="contenido" href={`https://mesaderegalos.liverpool.com.mx/milistaderegalos/${regalo[0].codigo}`}>
+            <div className="contenido">
                 <img className='icono img-fluid' src={regalo[0].icono} alt='...' />
                 <p className='codigo'>{regalo[0].modalidad}: {regalo[0].codigo}</p>
             </div>
