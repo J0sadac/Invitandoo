4b646eee4ff8896c926f3a701bdcdbc89c37cc66
Josadac Castaneda <jugdmentc12@gmail.com>
2024-06-23 20:52:10 -0600

    ...

--- src/components/Invitacion/components/modelo 3/mesa.js
+++ src/components/Invitacion/components/modelo 3/mesa.js
@@ -17,20 +17,21 @@ function Mesa ({fondo, mesa, confirmacion}) {
                 {mesa.explicacion}
             </p>
 
-            <Link className='regalo' to={`https://mesaderegalos.liverpool.com.mx/milistaderegalos/${mesa.codigo}`}>
-                <div className="contenido">
-                    <img className='icono' src={liverpool} alt='...' />
-                    <p className='codigo'>{mesa.modalidad}: {mesa.codigo}</p>
+            {confirmacion.lluvia === true && (
+                <div className='regalo' to={`https://mesaderegalos.liverpool.com.mx/milistaderegalos/51436895`}>
+                    <div className="contenido">
+                        <img className='icono' src={lluvia} alt='...' />
+                        <p className='codigo'>Lluvia de sobres</p>
+                    </div>
                 </div>
-
-                <p className="anuncio">¡Haz click aqui!</p>
-            </Link>
+            )}
 
             {mesa?.tarjeta && (
                 <>
                 <p className="gracias">
-                    si deseas otra opcion para hacerme un obsequio, 
-                    te agradecería una contribución a nuestra cuenta bancaria.
+                    Quiero ahorrarte tiempo y esfuerzo al elegir el regalo
+                    perfecto, si es tu elección obsequiarme algo, puedes hacerlo
+                    a travez de una transferencia bancaria
                 </p>
                 <div className='regalo' to={`https://mesaderegalos.liverpool.com.mx/milistaderegalos/51436895`}>
                     <div className="contenido">
@@ -45,14 +46,14 @@ function Mesa ({fondo, mesa, confirmacion}) {
                 </>
             )}
 
-            {confirmacion.lluvia === true && (
-                <div className='regalo' to={`https://mesaderegalos.liverpool.com.mx/milistaderegalos/51436895`}>
-                    <div className="contenido">
-                        <img className='icono' src={lluvia} alt='...' />
-                        <p className='codigo'>Lluvia de sobres</p>
-                    </div>
+            <Link className='regalo' to={`https://mesaderegalos.liverpool.com.mx/milistaderegalos/${mesa.codigo}`}>
+                <div className="contenido">
+                    <img className='icono' src={liverpool} alt='...' />
+                    <p className='codigo'>{mesa.modalidad}: {mesa.codigo}</p>
                 </div>
-            )}
+
+                <p className="anuncio">¡Haz click aqui!</p>
+            </Link>
 
             <p className="gracias">
                 Por supuesto, cualquier detalle será bienvenido y apreciado ¡Con cariño, muchas gracias!
