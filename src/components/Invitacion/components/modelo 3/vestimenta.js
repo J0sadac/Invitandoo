9c73230e9439686183370840c8e451fa78180b89
Josadac Castañeda <jugdmentc12@gmail.com>
2026-03-02 19:49:17 -0600

    Añadiendo ejemplos de vestimenta

--- src/components/Invitacion/components/modelo 3/vestimenta.js
+++ src/components/Invitacion/components/modelo 3/vestimenta.js
@@ -2,13 +2,13 @@
 import trajeEtiquetaOscuro from '../../../../multimedia/herramientas/traje etiqueta oscuro.png';
 import trajeFormalOscuro from '../../../../multimedia/herramientas/traje formal oscuro.png';
 import trajeSemifromalOscuro from '../../../../multimedia/herramientas/traje semi formal oscuro.png';
-import trajeInformalOscuro from '../../../../multimedia/herramientas/traje informal oscuro.png';
+import trajeInformalOscuro from '../../../../multimedia/herramientas/guayabera.png';
 
 //vestidos oscuros
 import vestidoEtiquetaOscuro from '../../../../multimedia/herramientas/vestido etiqueta oscuro.png';
 import vestidoFormalOscuro from '../../../../multimedia/herramientas/vestido formal oscuro.png';
 import vestidoSemifromalOscuro from '../../../../multimedia/herramientas/vestido semi formal oscuro.png';
-import vestidoInformalOscuro from '../../../../multimedia/herramientas/vestido informal oscuro.png';
+import vestidoInformalOscuro from '../../../../multimedia/herramientas/blusa pili.png';
 
 //trajes claros
 import trajeEtiquetaClaro from '../../../../multimedia/herramientas/traje etiqueta claro.png';
@@ -22,6 +22,14 @@ import vestidoFormalClaro from '../../../../multimedia/herramientas/vestido form
 import vestidoSemifromalClaro from '../../../../multimedia/herramientas/vestido semi formal claro.png';
 import vestidoInformalClaro from '../../../../multimedia/herramientas/vestido informal claro.png';
 
+//ejemplos de vestimenta - Alonso
+import vestMujer01 from '../../../../multimedia/herramientas/vestimenta-mujer-02.jpeg';
+import vestMujer02 from '../../../../multimedia/herramientas/vestimenta-mujer-03.jpeg';
+import vestMujer03 from '../../../../multimedia/herramientas/vestimenta-mujer-01.jpeg';
+import vestHombre01 from '../../../../multimedia/herramientas/vestimenta-hombre-02.jpeg';
+import vestHombre02 from '../../../../multimedia/herramientas/vestimenta-hombre-01.jpeg';
+import vestHombre03 from '../../../../multimedia/herramientas/vestimenta-hombre-03.jpeg';
+
 import tonoAzul from '../../../../multimedia/herramientas/tonos azul.png';
 import tonoRojo from '../../../../multimedia/herramientas/tonos rojo.png';
 import tonosHM from '../../../../multimedia/herramientas/tonos hombre y mujer.png';
@@ -31,11 +39,23 @@ import tonoCafe from '../../../../multimedia/herramientas/paleta dresscode.png';
 import tonoPastel from '../../../../multimedia/herramientas/tonos pastel.png';
 import codigoHam from '../../../../multimedia/herramientas/codigo ham.png';
 
-import icono from '../../../../multimedia/herramientas/adorno dorado.gif';
-import iconoDerecho from '../../../../multimedia/herramientas/Detalle Horizontal derecho.gif';
-import flor from '../../../../multimedia/herramientas/flor central.png';
+// import icono from '../../../../multimedia/herramientas/adorno dorado.gif';
+// import iconoDerecho from '../../../../multimedia/herramientas/Detalle Horizontal derecho.gif';
+// import flor from '../../../../multimedia/herramientas/flor central.png';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { useState } from 'react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/effect-cards';
+
+// import required modules
+import { EffectCards } from 'swiper/modules';
 
 function Vestimenta ({fondo, datos, festejado, modo}){
+    const [modalMujer, setModalMujer] = useState(false);
+    const [modalHombre, setModalHombre] = useState(false);
 
     return(
         <section className='vestimenta imprimir'>
@@ -43,13 +63,7 @@ function Vestimenta ({fondo, datos, festejado, modo}){
                 <img loading='lazy' className='fondo' src={fondo.url} alt='...' />
             )}
             <p className="titulo">Código de vestimenta</p>
-            {festejado === 'Consuelo & Andrés' && (
-                <div className='efecto'>
-                    <img src={icono} className='icono' alt='...' />
-                    <img src={flor} className='flor' alt='...' />
-                    <img src={iconoDerecho} className='icono' alt='...' />
-                </div>
-            )}
+            
             <>
                 {modo === 'oscuro' && (
                     <>
@@ -81,6 +95,7 @@ function Vestimenta ({fondo, datos, festejado, modo}){
                                 <p>{datos.codigo}</p>
                             </>
                         ):(
+                            //esta seccion chato
                             <>
                                 <div className='iconos'>
                                     <img loading='lazy' className="icono" src={trajeInformalOscuro} alt="..." />
@@ -88,6 +103,51 @@ function Vestimenta ({fondo, datos, festejado, modo}){
                                 </div>
 
                                 <p>{datos.codigo}</p>
+
+                                <div className="botonesReferencia">
+                                    <button onClick={() => setModalHombre(true)}>Ejemplos de caballeros</button>
+                                    <button onClick={() => setModalMujer(true)}>Ejemplos de damas</button>
+                                </div>
+
+                                {/* Modal Mujer */}
+                                {modalMujer && (
+                                    <div className="modalOverlay" onClick={() => setModalMujer(false)}>
+                                        <div className="modalContenido" onClick={(e) => e.stopPropagation()}>
+                                            <span className="cerrar" onClick={() => setModalMujer(false)}>×</span>
+
+                                            <Swiper
+                                                effect={'cards'}
+                                                grabCursor={true}
+                                                modules={[EffectCards]}
+                                                className="mySwiper"
+                                            >
+                                                <SwiperSlide><img src={vestMujer01} alt="..." /></SwiperSlide>
+                                                <SwiperSlide><img src={vestMujer02} alt="..." /></SwiperSlide>
+                                                <SwiperSlide><img src={vestMujer03} alt="..." /></SwiperSlide>
+                                            </Swiper>
+                                        </div>
+                                    </div>
+                                )}
+
+                                {/* Modal Hombre */}
+                                {modalHombre && (
+                                    <div className="modalOverlay" onClick={() => setModalHombre(false)}>
+                                        <div className="modalContenido" onClick={(e) => e.stopPropagation()}>
+                                            <span className="cerrar" onClick={() => setModalHombre(false)}>×</span>
+
+                                            <Swiper
+                                                effect={'cards'}
+                                                grabCursor={true}
+                                                modules={[EffectCards]}
+                                                className="mySwiper"
+                                            >
+                                                <SwiperSlide><img src={vestHombre01} alt="..." /></SwiperSlide>
+                                                <SwiperSlide><img src={vestHombre02} alt="..." /></SwiperSlide>
+                                                <SwiperSlide><img src={vestHombre03} alt="..." /></SwiperSlide>
+                                            </Swiper>
+                                        </div>
+                                    </div>
+                                )}
                             </>  
                         )}
                     </>
